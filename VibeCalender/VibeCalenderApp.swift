02fa6e0a22ec36bfacf233d5d3ceb0e1ea57bb74
kurazuuuuuu <110393398+kurazuuuuuu@users.noreply.github.com>
2025-12-17 09:41:31 +0900

    Initial Commit

---
+++ VibeCalender/VibeCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCalenderApp.swift
+//  VibeCalender
+//
+//  Created by Kanaha Noma on 2025/12/17.
+//
+
+import SwiftUI
+
+@main
+struct VibeCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
